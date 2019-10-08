307a597c5a0fa6f36294d17f505265abfa30093a
Damian <damianspizzirri@gmail.com>
2019-10-07 23:43:57 -0300

    agrega fases del ciclo de vida

--- src/app/componenteprueba/componenteprueba.component.ts
+++ src/app/componenteprueba/componenteprueba.component.ts
@@ -1,8 +1,9 @@
 import {
   Component,
   OnInit, OnChanges, DoCheck, OnDestroy, Input,
-  AfterContentInit,
-  SimpleChanges, ViewChild
+  AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked,
+  SimpleChanges, ViewChild, ContentChild
 } from '@angular/core';
 import { ComponentepruebahijoComponent } from '../componentepruebahijo/componentepruebahijo.component';
 
@@ -12,12 +13,16 @@ import { ComponentepruebahijoComponent } from '../componentepruebahijo/component
   styleUrls: ['./componenteprueba.component.css']
 })
 export class ComponentepruebaComponent
-  implements OnChanges, OnInit, DoCheck, OnDestroy, AfterContentInit {
+  implements OnChanges, OnInit, DoCheck, OnDestroy,
+  AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked {
 
   eventos: Array<String>;
 
   @Input("atributoHTML") atributo: String
-  @ViewChild(ComponentepruebahijoComponent, { static: false }) hijo: ComponentepruebahijoComponent;
+  @ViewChild(ComponentepruebahijoComponent, { static: false }) vistaHijo: ComponentepruebahijoComponent;
+  @ContentChild(ComponentepruebahijoComponent, { static: false }) contenidoHijo: ComponentepruebahijoComponent;
+
 
   constructor() {
     this.eventos = new Array();
@@ -46,9 +51,19 @@ export class ComponentepruebaComponent
     console.log("AfterContentInit: Se setea el componente hijo despues de que la vista se inicializo");
   }
 
+  ngAfterViewInit() {
+    this.agregarEvento("AfterViewInit");
+    console.log("AfterViewInit: Se setea el contenido del componente hijo despues de que la vista se inicializo");
+  }
+
   ngAfterViewChecked() {
     this.agregarEvento("AfterViewChecked");
     console.log("AfterViewChecked: Se actualiza el componente hijo despues de que la vista fue chequeda");
+  }
+
+  ngAfterContentChecked() {
+    this.agregarEvento("AfterContentChecked");
+    console.log("AfterContentChecked: Se actualiza el contenido del componente hijo despues de que la vista fue chequeda");
   }
 
   ngOnDestroy() {
